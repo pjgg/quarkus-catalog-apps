c3b717f7736da9beaadaebc13534f5472bb0226a
Jose Carvajal <josecarvajalhilario@gmail.com>
2020-11-17 09:22:51 +0100

    Removed unused Random import.

--- rest-api/src/test/java/io/quarkus/qe/RepositoryResourceTest.java
+++ rest-api/src/test/java/io/quarkus/qe/RepositoryResourceTest.java
@@ -5,7 +5,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Optional;
-import java.util.Random;
 
 import javax.enterprise.inject.Any;
 import javax.inject.Inject;
