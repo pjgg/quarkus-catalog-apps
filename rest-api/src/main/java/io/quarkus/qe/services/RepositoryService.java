e68f337d438c454cea37925ca61371a484c06c15
Jose Carvajal <josecarvajalhilario@gmail.com>
2020-11-17 09:23:43 +0100

    Removed unused imports.

--- rest-api/src/main/java/io/quarkus/qe/services/RepositoryService.java
+++ rest-api/src/main/java/io/quarkus/qe/services/RepositoryService.java
@@ -2,9 +2,6 @@ package io.quarkus.qe.services;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
-
-import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
-import io.quarkus.hibernate.orm.panache.PanacheQuery;
 import org.eclipse.microprofile.reactive.messaging.Channel;
 import org.eclipse.microprofile.reactive.messaging.Emitter;
 import io.quarkus.qe.configuration.Channels;
